b2b2fdee7392bca83da36d2ea535b9a125aa3e06
mshashank2002 <mshashank8867@gmail.com>
2023-07-22 11:08:30 +0530

    Added To KrsihiMitra

---
+++ contact.php
@@ -0,0 +1,82 @@
+<?php
+include("header.php");
+?>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>CONTACT US...</h2>
+          <ol>
+            <li>Happy to help..</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+	
+
+ 
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container">
+
+        <div class="text-center" data-aos="zoom-in">
+          <h3>We'd Love To Hear From You!!!</h3>
+          <p> Please use the contact form on the right side if you have any questions or requests, concerning our services. <br>We will respond to your message within 24 hours.</p>
+    
+        <a href="#" class="twitter cta-btn "><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook cta-btn"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram cta-btn"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus cta-btn"><i class="bx bxl-skype"></i></a>
+        <a href="https://www.linkedin.com/in/m-shashank-657144235/" class="linkedin cta-btn"><i class="bx bxl-linkedin"></i></a>
+
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-12" data-aos="fade-up" data-aos-delay="100">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3885.4599318391934!2d77.5673768!3d13.133363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae18a5c54ece35%3A0x49c4c47a68a60b9c!2sBMS%20Institute%20of%20Technology%20and%20Management!5e0!3m2!1sen!2sin!4v1689958014894!5m2!1sen!2sin" width="1000" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="info mt-4">
+              <i class="icofont-google-map"></i>
+              <h4>Location:</h4>
+              <p>BMSIT Avalahalli, Yelahanka. Bangalore -560064</p>
+            </div>
+            <div class="row">
+              <div class="col-lg-6 mt-4">
+                <div class="info">
+                  <i class="icofont-envelope"></i>
+                  <h4>Email:</h4>
+                  <p>mshashank8867@gmail.com</p>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="info w-100 mt-4">
+                  <i class="icofont-phone"></i>
+                  <h4>Call:</h4>
+                  <p>+91-8867283194</p>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+  
+<?php
+include("footer.php");
+?>
